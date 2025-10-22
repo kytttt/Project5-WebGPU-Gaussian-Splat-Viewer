5539dd906b39f9d48c24a4d5130240bd17d3416b
kytttt <kbyt824@gmail.com>
2025-10-22 11:02:27 -0400

    part1

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,14 @@ import rawPlugin from 'vite-raw-plugin';
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+    server: {
+        open: true,
+    },
     build: {
         target: 'esnext'
     },
-    base: process.env.GITHUB_ACTIONS_BASE || undefined,
+    // Avoid TypeScript Node type requirement by accessing via globalThis
+    base: (globalThis as any)?.process?.env?.GITHUB_ACTIONS_BASE || undefined,
     plugins: [
         rawPlugin({
             fileRegex: /\.wgsl$/,
